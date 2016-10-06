7a60b12fde484413701946b9ec9e528bf0ce1dd8
Wolfgang Criollo <wolfplisken@gmail.com>
2016-10-06 18:23:56 -0400

    set up sass and css modules loaders to work with project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
-import Header from './components/header'
+import Header from './components/header';
+require('../styles/styles.js');
 export default class App extends Component{
   render(){
     return(

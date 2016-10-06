8fe9277ef345372cc8160a90a17749d561250513
Wolfgang Criollo <wolfplisken@gmail.com>
2016-10-06 16:37:15 -0400

    fix hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,17 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  // devtool:"inline-source-map",
-  devtool:"eval",
-  entry:'./src/index.js',
-  // [
-  //   'webpack-dev-server/client?http://127.0.0.1:8080/',
-  //   'webpack/hot/only-dev-server',
-  //   './src'
-  // ],
+  // devtool:"eval",
+  // entry:'./src/index.js',
+  devtool:"inline-source-map",
+  entry:[
+    'webpack-dev-server/client?http://127.0.0.1:8080/',
+    'webpack/hot/only-dev-server',
+    './src'
+  ],
   output:{
     path:path.join(__dirname,'src'),
-    filename:'bundle.js'
+    filename:'src/bundle.js'
   },
   resolve:{
     moduleDirectories:['node_modules','src'],

d94941968d45441a4147ea532678aa6dc395b983
Wolfgang Criollo <wolfplisken@gmail.com>
2016-10-05 22:33:59 -0400

    set up webpack and express server

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+import Header from './components/header'
+export default class App extends Component{
+  render(){
+    return(
+      <div id="main">
+        <Header />
+      </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool:"inline-source-map",
+  entry:[
+    'webpack-dev-server/client?http://127.0.0.1:8080/',
+    'webpack/hot/only-dev-server',
+    './src'
+  ],
+  output:{
+    path:path.join(__dirname),
+    filename:'src/bundle.js'
+  },
+  resolve:{
+    moduleDirectories:['node_modules','src'],
+    extensions:['','.js']
+  },
+  module:{
+    loaders:[
+      {
+      test:/\.jsx?$/,
+      exclude: /node_modules/,
+      loaders: [ 'babel?presets[]=react,presets[]=es2015']
+    }
+  ]
+  },
+  plugins:[
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+};

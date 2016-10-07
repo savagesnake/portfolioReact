b2d336cbef57f14288e0f17b4a5147648e407b92
Wolfgang Criollo <wolfplisken@gmail.com>
2016-10-07 15:46:42 -0400

    setup the background image for header set up grid viewport set break point for desktop and phone size , set mixin for mediaqueries on sass

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,12 @@ export default class Header extends Component{
   render(){
     return(
       <div className="header-wrap header-bg">
-        <div className="header-content">
-        <img  className="icon" src="../public/images/wolfgangIcon.png" alt="wolfgang Criollo"/>
-        <h1>My name is Wolfgang A Criollo</h1>
-        <h2>I'm a webdesigner & developer</h2>
+        <div className="header-inner-wrap">
+          <div className="header-content">
+          <img  className="icon" src="../public/images/wolfgangIcon.png" alt="wolfgang Criollo"/>
+          <h1>My name is Wolfgang A Criollo</h1>
+          <h2>I'm a webdesigner & developer</h2>
+          </div>
         </div>
       </div>
     )

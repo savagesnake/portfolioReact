58c13ab4c1b212282f1edc106bc607519f588825
Wolfgang Criollo <wolfplisken@gmail.com>
2016-10-08 16:43:04 -0400

    update webpack config path to make more dry

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
 var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var srcPath = path.join(__dirname,"src");
+var builPath = path.join(__dirname,"/public");
 
 
 module.exports = {
-  devtool:"eval",
-  entry:'./src/index.js',
-  // devtool:"inline-source-map",
-  // entry:[
-  //   'webpack-dev-server/client?http://127.0.0.1:8080/',
-  //   'webpack/hot/only-dev-server',
-  //   './src'
-  // ],
+  // devtool:"eval",
+  // entry:path.join(srcPath,"index.js"),
+  devtool:"inline-source-map",
+  entry:[
+    'webpack-dev-server/client?http://127.0.0.1:8080/',
+    'webpack/hot/only-dev-server',
+    './src'
+  ],
   output:{
     path:path.join(__dirname,'/public'),
     filename:'./src/bundle.js'

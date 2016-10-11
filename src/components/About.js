93735f7146503ce1a9b2b856a2629e161e82caa3
Wolfgang Criollo <wolfplisken@gmail.com>
2016-10-11 00:46:12 -0400

    update about info text

--- src/components/About.js
+++ src/components/About.js
@@ -10,11 +10,10 @@ export default class About extends Component{
             <div className="lead">
             <img className="profilePic" src={profilePic} alt="profile-image" title="Wolfgang Criollo"/>
                 <p>
-                Love CSS, making websites presentable is my passion, adding functionality with javascript, Jquery to bring it to live just makes my day
-                I started with interest in 3D design after
-                my undergraduate, I started to focus more
-                in graphic and web design and after working for 3yr as a web designer
-                I realise that front-end is my passion, I desided to attend Devbootcamp to get upto date with technology and become a fullstack developer but focusing on frond-end-development
+                I'm a determined individual  that loves developing websites.
+                I self-thought myself most of  the many skills I have and have improved those skills by attending to college and other tech schools.
+                after many years I have found my real passion in front-end development at the moment I'm focusing  strengthening my skills on the many different frameworks that are out there,
+                currently  looking for opportunities to work at a place that will allow my skill to grow, I'm always looking to challenge myself.
                 </p>
             </div>
           </div>

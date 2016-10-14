585efbc7bfd2266aba55199b06694145c9b12aa9
Wolfgang A Criollo <wolfplisken@gmail.com>
2016-10-14 03:53:37 -0400

    build podution version to deploy heroku update

--- webpack.config.js
+++ webpack.config.js
@@ -5,18 +5,18 @@ var srcPath = path.join(__dirname,"src");
 var builPath = path.join(__dirname,"/public");
 
 module.exports = {
-  // devtool:"eval",
-  // entry:path.join(srcPath,"index.js"),
-  devtool:"inline-source-map",
-  entry:[
-    'webpack-dev-server/client?http://127.0.0.1:8080/',
-    'webpack/hot/only-dev-server',
-    './src'
-  ],
+  devtool:"eval",
+  entry:path.join(srcPath,"index.js"),
+  // devtool:"inline-source-map",
+  // entry:[
+  //   'webpack-dev-server/client?http://127.0.0.1:8080/',
+  //   'webpack/hot/only-dev-server',
+  //   './src'
+  // ],
   output:{
     path:builPath,
-    filename:'./src/bundle.js',
-    publicPath:'/public/'
+    filename:'./src/bundle.js'
+    // publicPath:'/public/'
   },
   resolve:{
     moduleDirectories:['node_modules','src'],
